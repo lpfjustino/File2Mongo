b0823aa53b4a150ce4e6c07fec0ec57cf6f91e5a
lpfjustino <lpfjustino@gmail.com>
2017-07-08 18:24:28 -0300

    Script refactored to work with any collection

--- main.py
+++ main.py
@@ -3,22 +3,31 @@ import time
 import json
 
 from pymongo import MongoClient
+from pymongo.errors import *
 
 # Directory settings
-directory = 'F:\RUBoosted\\new_matches'
-pathlist = Path(directory).glob('**/*.txt')
+matches_folder = 'F:\RUBoosted\\new_matches'
+summoners_folder = 'D:\__NOTE\Documentos\RUBoosted\summoners'
+
 
 # MongoDB Driver settings
 client = MongoClient()
 db = client['RUBoosted']
 
-for path in pathlist:
-    # because path is object not string
-    path_in_str = str(path)
-    print(path_in_str)
+def insert_to_collection(directory, collection):
+    pathlist = Path(directory).glob('**/*.txt')
+    for path in pathlist:
+        # because path is object not string
+        path_in_str = str(path)
 
-    with open(path_in_str, mode="r") as f:
+        f = open(path_in_str, mode="r")
         match = json.load(f)
-        db.matches.insert_one(match)
+        try:
+            db[collection].insert_one(match)
+            print('Successfully inserted \t', path_in_str)
 
-        time.sleep(10)
+        except DuplicateKeyError:
+            print(collection + ' already contains this document:\t', path_in_str)
+
+insert_to_collection(summoners_folder, 'summoners')
+insert_to_collection(matches_folder, 'matches')
